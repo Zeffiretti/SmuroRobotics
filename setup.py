95791588691ddf12faa184dd3789a3809a59f415
Zeffiretti <hiesh@mail.com>
2021-11-01 17:09:45 +0800

    make it a package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+
+setuptools.setup(
+    name='smuro_robotics',
+    version='0.0.1',
+    author='Zeffiretti Hiesh',
+    author_email='hiesh@mail.com',
+    description='Smuro Robotics Caculation Package',
+    long_description="long_description",
+    long_description_content_type="text",
+    url='https://github.com/zeffiretti/SmuroRobotics',
+    project_urls = {
+        "Bug Tracker": "https://github.com/zeffiretti/SmuroRobotics/issues"
+    },
+    license='GNU Lesser General Public License v2.1',
+    packages=['smuro_robotics'],
+    install_requires=['numpy','modern_robotics'],
+)
