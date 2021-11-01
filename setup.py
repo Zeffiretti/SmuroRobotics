fe9b60833b3ea210cc49c91f7e91d5053b359c13
Zeffiretti <hiesh@mail.com>
2021-11-01 19:10:12 +0800

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name='smuro_robotics',
-    version='0.0.1',
+    version='0.0.2',
     author='Zeffiretti Hiesh',
     author_email='hiesh@mail.com',
     description='Smuro Robotics Caculation Package',
