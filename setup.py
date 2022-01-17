0d888093b19bad62158bf0d1d40e1e25b5bb46d3
Zeffiretti <hiesh@mail.com>
2022-01-17 19:22:33 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 import setuptools
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+# long_description = ""
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_desc = fh.read()
 
 setuptools.setup(
     name='smuro_robotics',
     version='0.0.3',
     author='Zeffiretti Hiesh',
     author_email='hiesh@mail.com',
-    description='Smuro Robotics Caculation Package',
-    long_description="long_description",
+    description='Smuro Robotics Calculation Package',
+    long_description=long_desc,
     long_description_content_type="text",
     url='https://github.com/zeffiretti/SmuroRobotics',
-    project_urls = {
+    project_urls={
         "Bug Tracker": "https://github.com/zeffiretti/SmuroRobotics/issues"
     },
     license='GNU Lesser General Public License v2.1',
     packages=['smuro_robotics'],
-    install_requires=['numpy','modern_robotics'],
+    install_requires=['numpy', 'modern_robotics'],
 )
